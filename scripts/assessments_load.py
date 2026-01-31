8327372b22ee88bbab4e52548246141dcd0293b3
Youssof Elroweini <elroweiniyoussof@gmail.com>
2026-01-31 04:17:10 -0500

    added scores data loading script

---
+++ scripts/assessments_load.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pathlib import Path
+
+PATH = Path(__file__).parent.parent
+
+assessments = pd.read_csv(f'{PATH}/data/assessments.csv')
+studentAssessment = pd.read_csv(f'{PATH}/data/studentAssessment.csv')
+vle = pd.read_csv(f'{PATH}/data/vle.csv')
+studentsVle = pd.read_csv(f'{PATH}/data/studentVle.csv')
+studentInfo = pd.read_csv(f'{PATH}/data/studentInfo.csv')
+
+final_df = studentAssessment.merge(assessments, how='left', on='id_assessment')
+
+final_df["score"] = (final_df["score"]/100) * final_df["weight"]
+exams = final_df[final_df['assessment_type'] == 'Exam']
+assignments = final_df[final_df['assessment_type'] != 'Exam']
+
+
+assignments = assignments.groupby(['code_module','code_presentation','id_student'])['score'].sum()
+exams = exams.groupby(['code_module','code_presentation','id_student'])['score'].sum()
+
+merged = pd.merge(assignments, exams , how='left', on=['code_module','code_presentation','id_student'])
+
+vle_merged = studentsVle.merge(vle, how='left', on=["id_site", "code_module", "code_presentation"])
+print(vle_merged.head())
+vle_agg = vle_merged.groupby(["id_student", "code_module", "code_presentation"]).agg(total_clicks=("sum_click", "sum"),activity_diversity=( "activity_type","nunique")).reset_index()
+
+final_df = merged.merge(vle_agg, how="left", on=["id_student", "code_module", "code_presentation"])
+final_df = final_df.merge(studentInfo, how="left", on=["code_module", "code_presentation","id_student"])
+#final_df = final_df.groupby(['code_module','code_presentation','id_student']).agg(final_score = ("score","sum"),is_banked = ("is_banked","max"))
+#final_df.to_csv(f'{PATH}/data/data_assessment.csv', index=True)
+final_df = final_df[final_df['final_result'] != "Withdrawn"]
+
+#final_df = final_df.drop(["final_result"],axis=1)
+
+final_df = final_df.rename({"score_x":'assignment_score',"score_y":"exam_score"},axis=1)
+
+final_df.to_csv(f'{PATH}/data/regression_df.csv', index=False)
+
+print(final_df.head())
