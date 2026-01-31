82ff56034675193ae9e33e6b4e616a713a2393e7
Youssof Elroweini <elroweiniyoussof@gmail.com>
2026-01-31 06:07:13 -0500

    fixed regression data

--- scripts/assessments_load.py
+++ scripts/assessments_load.py
@@ -1,3 +1,5 @@
+from typing import final
+
 import pandas as pd
 from pathlib import Path
 
@@ -8,32 +10,32 @@ studentAssessment = pd.read_csv(f'{PATH}/data/studentAssessment.csv')
 vle = pd.read_csv(f'{PATH}/data/vle.csv')
 studentsVle = pd.read_csv(f'{PATH}/data/studentVle.csv')
 studentInfo = pd.read_csv(f'{PATH}/data/studentInfo.csv')
+studentRegistration = pd.read_csv(f'{PATH}/data/studentRegistration.csv')
 
-final_df = studentAssessment.merge(assessments, how='left', on='id_assessment')
+base = studentInfo.merge(studentRegistration, how='left', on=["id_student", "code_module", "code_presentation"])
 
-final_df["score"] = (final_df["score"]/100) * final_df["weight"]
-exams = final_df[final_df['assessment_type'] == 'Exam']
-assignments = final_df[final_df['assessment_type'] != 'Exam']
+assessments = studentAssessment.merge(assessments, how='left', on='id_assessment')
+assessments['score'] = (assessments['score']/100) * assessments['weight']
+
+scores = assessments.groupby(['code_module','code_presentation','id_student'])['score'].sum().reset_index()
 
 
-assignments = assignments.groupby(['code_module','code_presentation','id_student'])['score'].sum()
-exams = exams.groupby(['code_module','code_presentation','id_student'])['score'].sum()
+final_df = base.merge(scores, how='left', on=['id_student', 'code_module', 'code_presentation'])
 
-merged = pd.merge(assignments, exams , how='left', on=['code_module','code_presentation','id_student'])
+merged = final_df.copy()
 
 vle_merged = studentsVle.merge(vle, how='left', on=["id_site", "code_module", "code_presentation"])
 print(vle_merged.head())
 vle_agg = vle_merged.groupby(["id_student", "code_module", "code_presentation"]).agg(total_clicks=("sum_click", "sum"),activity_diversity=( "activity_type","nunique")).reset_index()
 
 final_df = merged.merge(vle_agg, how="left", on=["id_student", "code_module", "code_presentation"])
-final_df = final_df.merge(studentInfo, how="left", on=["code_module", "code_presentation","id_student"])
 #final_df = final_df.groupby(['code_module','code_presentation','id_student']).agg(final_score = ("score","sum"),is_banked = ("is_banked","max"))
 #final_df.to_csv(f'{PATH}/data/data_assessment.csv', index=True)
+
 final_df = final_df[final_df['final_result'] != "Withdrawn"]
 
 #final_df = final_df.drop(["final_result"],axis=1)
 
-final_df = final_df.rename({"score_x":'assignment_score',"score_y":"exam_score"},axis=1)
 
 final_df.to_csv(f'{PATH}/data/regression_df.csv', index=False)
 
