5d224f4a8273323f01aec4d30bd087d0353510b6
ah3asef <an6161080@gmail.com>
2026-01-31 09:21:06 +0200

    anything

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # hackthon
 
-## Features Description
-
-`Date_Unregistersation:` ***The Day that Students has unrolled the course after the course begun.***
 
 
--- scripts/load_merge_test.py
+++ scripts/load_merge_test.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
 
     PATH = Path(__file__).parent.parent
 
-    students_assessments = pd.read_csv(f'{PATH}/data/studentAssessment.csv')
-    student_info = pd.read_csv(f'{PATH}/data/studentInfo.csv')
-    courses = pd.read_csv(f'{PATH}/data/courses.csv')
-    assessments = pd.read_csv(f'{PATH}/data/assessments.csv')
-    student_registration = pd.read_csv(f'{PATH}/data/studentRegistration.csv')
-    vle = pd.read_csv(f'{PATH}/data/vle.csv')
-    studentsVle = pd.read_csv(f'{PATH}/data/studentVle.csv')
+    students_assessments = pd.read_csv(f'{PATH}/data/anonymiseddata/studentAssessment.csv')
+    student_info = pd.read_csv(f'{PATH}/data/anonymiseddata/studentInfo.csv')
+    courses = pd.read_csv(f'{PATH}/data/anonymiseddata/courses.csv')
+    assessments = pd.read_csv(f'{PATH}/data/anonymiseddata/assessments.csv')
+    student_registration = pd.read_csv(f'{PATH}/data/anonymiseddata/studentRegistration.csv')
+    vle = pd.read_csv(f'{PATH}/data/anonymiseddata/vle.csv')
+    studentsVle = pd.read_csv(f'{PATH}/data/anonymiseddata/studentVle.csv')
 
 
     base = student_info.merge(student_registration, how='left', on=["id_student", "code_module", "code_presentation"])
@@ -22,14 +22,13 @@ if __name__ == "__main__":
 
 
     base = base.merge(assessments_agg, how='left', on=["id_student","code_module","code_presentation"])
-    
-    base = base.merge(courses,how = 'left', on = ['code_module','code_presentation'])
 
-    vle_merged = studentsVle.merge(vle, how='left', on=["id_site","code_module","code_presentation"])
+
+    vle_merged = studentsVle.merge(vle.drop(["code_module","code_presentation"],axis = 1), how='left', on=["id_site"])
     print(vle_merged.head())
     vle_agg = vle_merged.groupby(["id_student", "code_module", "code_presentation"]).agg(total_clicks=("sum_click", "sum"),activity_diversity=("activity_type", "nunique")).reset_index()
 
     final_df = base.merge(vle_agg, how='left', on=["id_student", "code_module", "code_presentation"])
-    final_df.reset_index().to_csv(f'{PATH}/data/final_vle_2.csv', index=False)
+    final_df.reset_index().to_csv(f'{PATH}/data/anonymiseddata/final_vle_test.csv', index=False)
 
     
